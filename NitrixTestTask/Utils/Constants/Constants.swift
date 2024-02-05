5a7da2e284df86fa750760d36414b16ecc182bd3
OlehOliinykov <oleh38568@gmail.com>
2024-02-05 10:26:14 +0200

    Added Constants file

---
+++ NitrixTestTask/Utils/Constants/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  NitrixTestTask
+//
+//  Created by Oleh Oliinykov on 01.02.2024.
+//
+
+import Foundation
+import UIKit
+
+enum Constants {
+    static let screenSize: CGSize = UIScreen.main.bounds.size
+}
